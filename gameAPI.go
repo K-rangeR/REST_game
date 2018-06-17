28c44d323da639df6412d93a93d40f013ee22355
Kyle Ranger <zranger2345@gmail.com>
2018-06-16 21:03:04 -0500

    Add gorilla mux routing to replace the old way

--- gameAPI.go
+++ gameAPI.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-	"path"
+
+	"github.com/gorilla/mux"
 )
 
 // Game represents some data that is part of a vidio game
@@ -32,7 +34,8 @@ func handleAdd(w http.ResponseWriter, r *http.Request) {
 // handleGet will search the DB for the specified game title
 // if found it will return json containing the game data
 func handleGet(w http.ResponseWriter, r *http.Request) {
-	gameTitle := path.Base(r.URL.Path)
+	vars := mux.Vars(r)
+	gameTitle := vars["title"]
 	game, ok := games[gameTitle]
 	if ok {
 		jsonData, err := json.MarshalIndent(&game, "", "\t")
@@ -52,7 +55,8 @@ func handleGet(w http.ResponseWriter, r *http.Request) {
 
 // handleDelete will remove the specified game from the database
 func handleDelete(w http.ResponseWriter, r *http.Request) {
-	gameTitle := path.Base(r.URL.Path)
+	vars := mux.Vars(r)
+	gameTitle := vars["title"]
 	if _, ok := games[gameTitle]; ok {
 		delete(games, gameTitle)
 		w.WriteHeader(200)
@@ -66,11 +70,16 @@ var games map[string]Game
 
 func main() {
 	games = make(map[string]Game)
-	server := http.Server{
-		Addr: "127.0.0.1:8080",
-	}
-	http.HandleFunc("/gameAPI/add", handleAdd)
-	http.HandleFunc("/gameAPI/get/", handleGet)
-	http.HandleFunc("/gameAPI/delete/", handleDelete)
-	server.ListenAndServe()
+
+	// mock data
+	games["skyrim"] = Game{"skyrim", "bethesda", "M"}
+	games["2k"] = Game{"2k", "2k games", "E"}
+	games["FIFA18"] = Game{"FIFA18", "EA", "E"}
+	games["fallout3"] = Game{"fallout3", "bethesda", "M"}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/gameAPI/add", handleAdd).Methods("POST")
+	r.HandleFunc("/gameAPI/get/{title}", handleGet).Methods("GET")
+	r.HandleFunc("/gameAPI/delete/{title}", handleDelete).Methods("DELETE")
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080", r))
 }
